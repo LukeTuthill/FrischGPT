40c732bdeb8fdc4a95233e2b38bc535537c0f581
Luke Tuthill <98064685+LukeTuthill@users.noreply.github.com>
2023-05-17 09:26:58 -0400

    Add files via upload

---
+++ basicFunctions.ts
@@ -0,0 +1,92 @@
+import * as vscode from 'vscode';
+import {ChatGPT} from './ChatGPT';
+
+let chatgpt:ChatGPT;
+
+export function setGPT(apikey:string) {
+    chatgpt = new ChatGPT(apikey);
+}
+
+export function askQuestion(question:string) {
+    return chatgpt.askQuestion(question);
+}
+
+export function editText(text:string, prompt?:string) {
+    if (prompt !== undefined) {
+        return chatgpt.fixCode(text, prompt);
+    }
+    return chatgpt.fixCode(text);
+}
+
+export function explainCode(text:string, isComment:boolean) {
+    if (isComment) {
+    return chatgpt.explainCode(text, true);
+    }
+    return chatgpt.explainCode(text, false);
+}
+
+//Has an optional position parameter, defaults to current cursor position
+export function enterText(text: string, position?:vscode.Position) {
+    const editor = vscode.window.activeTextEditor;
+    if (editor) {
+        editor.edit(editBuilder => {
+            if (position === undefined) {
+                editBuilder.insert(editor.selection.active, text);
+            }
+            else {
+                editBuilder.insert(position, text);
+            }
+        });
+    }
+}
+
+export function replaceText(text:string) {
+    const editor = vscode.window.activeTextEditor;
+    if (editor) {
+        editor.edit(editBuilder => {
+            const selection = editor.selection;
+            if (selection && !selection.isEmpty) {
+                const range = new vscode.Range(selection.start.line, selection.start.character, 
+                    selection.end.line, selection.end.character);
+                editBuilder.delete(range);
+            }
+            editBuilder.insert(editor.selection.active, text);
+        });
+    }
+}
+
+export function getHighlightedText() {
+    let highlighted:string = "";
+    const editor = vscode.window.activeTextEditor;
+    if (editor !== undefined) {
+        const selection = editor.selection;
+        if (selection && !selection.isEmpty) {
+            const range = new vscode.Range(selection.start.line, selection.start.character, 
+                selection.end.line, selection.end.character);
+            highlighted = editor.document.getText(range);
+        }
+    }
+    return highlighted;
+}
+
+// export function getLanguage() {
+//     const editor = vscode.window.activeTextEditor;
+//     let fileName:string = "";
+//     if (editor !== undefined) {
+//         fileName = editor.document.fileName;
+//     }
+//     let splitFile = fileName.split(".");
+//     return splitFile[splitFile.length-1];
+// }
+
+
+// export function findPosition() {
+//     const editor = vscode.window.activeTextEditor;
+//     if (editor !== undefined) {
+//         const text = editor.document;
+//         const lineCount = text.lineCount;
+//         for (let line = 0; line < lineCount; line++) {
+            
+//         }
+//     }
+// }
---
+++ extension.ts
@@ -0,0 +1,80 @@
+import * as vscode from 'vscode';
+import * as functions from './basicFunctions';
+
+export function activate(context: vscode.ExtensionContext) {
+	console.log('WARNING: FRISCH GPT HAS BEEN ACTIVATED');
+
+	let initalCommand = vscode.commands.registerCommand('frischgpt.inputAPIKey', () => {
+		let apikey = vscode.window.showInputBox({prompt: "ChatGPT Api key:"});
+		apikey.then((a) => {
+			if (a !== undefined) {
+				functions.setGPT(a);
+			}
+		});
+	});
+
+	let command1 = vscode.commands.registerCommand('frischgpt.helloWorld', () => {
+		vscode.window.showInformationMessage('Any comments, questions, concerns, or cries of outrage?');
+	});
+
+	let command2 = vscode.commands.registerCommand('frischgpt.askQuestion', () => {
+		let question = vscode.window.showInputBox();
+		question.then((q) => {
+			if (q !== undefined) {
+				let response = functions.askQuestion(q);
+				response.then((r) => {
+					if (r.data.choices[0].text !== undefined) {
+						vscode.window.showInformationMessage(r.data.choices[0].text);
+					}
+				});
+			}
+		});
+	});
+
+	let command3 = vscode.commands.registerCommand('frischgpt.fixCode', () => {
+		let response = functions.editText(functions.getHighlightedText());
+		response.then((r) => {
+			if (r.data.choices[0].text !== undefined) {
+				functions.replaceText(r.data.choices[0].text);
+			}
+		});
+	});
+
+	let command4 = vscode.commands.registerCommand('frischgpt.fixCodeWithPrompt', () => {
+		let prompt = vscode.window.showInputBox({prompt:"Prompt:"});
+			prompt.then((p) => {
+				let response = functions.editText(functions.getHighlightedText(), p);
+				response.then((r) => {
+				if (r.data.choices[0].text !== undefined) {
+					functions.replaceText(r.data.choices[0].text);
+				}
+			});
+		});
+	});
+
+	let command5 = vscode.commands.registerCommand('frischgpt.explainCode', () => {
+		let choice = vscode.window.showQuickPick(["Explanation as comments in code", "Explanation separate"]);
+		choice.then((c)=> {
+			if (c === "Explanation as comments in code") {
+				let response = functions.explainCode(functions.getHighlightedText(), true);
+				response.then((r) => {
+					if (r.data.choices[0].text !== undefined) {
+						functions.replaceText(r.data.choices[0].text);
+					}
+				});
+			}
+			else if (c === "Explanation separate") {
+				let response = functions.explainCode(functions.getHighlightedText(), false);
+				response.then((r) => {
+					if (r.data.choices[0].text !== undefined) {
+						vscode.window.showInformationMessage(r.data.choices[0].text);
+					}
+				});
+			}
+		});
+	});
+
+	context.subscriptions.push(command1);
+}
+
+export function deactivate() {}
